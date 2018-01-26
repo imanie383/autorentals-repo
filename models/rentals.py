5083b9c58b2ab60c22a2f1a285f3783658ea7ad1
Manuel T. Vauxoo <manuel@vauxoo.com>
2018-01-25 18:05:40 -0600

    added calculate prices and rentals xml form

--- models/rentals.py
+++ models/rentals.py
@@ -3,11 +3,10 @@
 
 from odoo import models, fields, api
 import time
-from datetime import timedelta
+from datetime import datetime, date, time
 
 
 class autorentas(models.Model):
-
     _name = 'autorentas.rental'
 
     vehicle_id = fields.Many2one('autorentas.vehicle')
@@ -16,13 +15,25 @@ class autorentas(models.Model):
     date_start = fields.Date()
     date_end = fields.Date()
 
-    total_price = fields.Float()
+    total_price = fields.Char()
 
     @api.onchange('date_start', 'date_end', 'vehicle_id')
     def calculate_price(self):
+        if self.date_end and self.date_start:
+            # Read dates
+            d0 = datetime.strptime(self.date_start,'%Y-%m-%d')
+            d1 = datetime.strptime(self.date_end,'%Y-%m-%d')
+            
+            # Get the days number
+            n_days = d1 - d0
+            n_days = str(n_days)
+            n_days = n_days.split(' ')
+            n_days = n_days[0]
+            n_days = float(n_days)
+            
+            # Get price by day
+            a_day = self.vehicle_id.price
+            a_day  = float(a_day)
 
-        # return vehiculo.precio * (dia_fin-dia_inicio
-
-        a_day = self.vehicle_id.price
-        delta = self.date_end - self.date_start
-        print(delta)
+            #Get and write the total
+            self.total_price = str(a_day*n_days)
